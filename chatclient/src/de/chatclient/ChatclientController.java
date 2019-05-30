c09e8bdbb48f4198493895d70e24a1f3d8c16af3
Jonas <j-senger@gmx.de>
2019-05-30 23:32:45 +0200

    Commit.

--- chatclient/src/de/chatclient/ChatclientController.java
+++ chatclient/src/de/chatclient/ChatclientController.java
@@ -6,6 +6,7 @@ package de.chatclient;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.net.URL;
+import java.util.HashMap;
 import java.util.ResourceBundle;
 
 import javax.imageio.ImageIO;
@@ -14,19 +15,29 @@ import de.Main;
 import de.networking.logger.Logger;
 import de.types.Contact;
 import de.types.ContactViewCell;
+import javafx.animation.Animation;
+import javafx.animation.FillTransition;
+import javafx.animation.Interpolator;
+import javafx.animation.Transition;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.scene.control.ListView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.Region;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Circle;
 import javafx.scene.text.Text;
+import javafx.util.Duration;
 
 /**
  * FXML Controller class
@@ -52,7 +63,10 @@ public class ChatclientController implements Initializable
 
 	@FXML
 	Text status;
-	
+
+	@FXML
+	Pane close_bg;
+
 	@FXML
 	Circle online_status;
 
@@ -110,6 +124,79 @@ public class ChatclientController implements Initializable
 	}
 
 	@FXML
+	private void hover_minimize(MouseEvent event)
+	{
+		animate(c0, c1, (Region) event.getSource());
+	}
+
+	@FXML
+	private void leave_minimize(MouseEvent event)
+	{
+		if (animations.containsKey(event.getSource()))
+		{
+			animations.get(event.getSource()).stop();
+			animations.remove(event.getSource());
+			((Region) event.getSource()).setBackground(
+					new Background(new BackgroundFill(Color.rgb(34, 34, 34), CornerRadii.EMPTY, Insets.EMPTY)));
+			animate(c1, c0, (Region) event.getSource());
+		}
+	}
+
+	Color c0 = Color.rgb(34, 34, 34);
+	Color c1 = Color.rgb(85, 85, 85);
+	Color c2 = Color.rgb(255, 51, 51);
+
+	@FXML
+	private void hover_close(MouseEvent event)
+	{
+
+		animate(c0, c2, close_bg);
+	}
+
+	@FXML
+	private void leave_close(MouseEvent e)
+	{
+		if (animations.containsKey(close_bg))
+		{
+			animations.get(close_bg).stop();
+			animations.remove(close_bg);
+			animate(c2, c0, close_bg);
+		}
+	}
+
+	private HashMap<Region, Animation> animations = new HashMap<>();
+
+	private void animate(Color start, Color end, Region target)
+	{
+		if (animations.containsKey(target))
+		{
+			animations.get(target).stop();
+			animations.remove(target);
+		}
+		final Animation animation = new Transition()
+		{
+			{
+				setCycleDuration(Duration.millis(40));
+				setInterpolator(Interpolator.EASE_OUT);
+			}
+
+			@Override
+			protected void interpolate(double frac)
+			{
+				Color vColor = new Color((start.getRed() + ((end.getRed() - start.getRed()) * frac)),
+						(start.getGreen() + ((end.getGreen() - start.getGreen()) * frac)),
+						(start.getBlue() + ((end.getBlue() - start.getGreen()) * frac)),
+						(start.getOpacity() + ((end.getOpacity() - start.getOpacity()) * frac)));
+
+				target.setBackground(new Background(new BackgroundFill(vColor, CornerRadii.EMPTY, Insets.EMPTY)));
+			}
+		};
+		animation.setCycleCount(1);
+		animation.play();
+		animations.put(target, animation);
+	}
+
+	@FXML
 	private void minimize(MouseEvent event)
 	{
 		Main.getInstance().getPrimaryStage().setIconified(true);
@@ -140,7 +227,7 @@ public class ChatclientController implements Initializable
 		profilepic.setFill(new ImagePattern(SwingFXUtils.toFXImage(bi, null)));
 		profilepic.setStyle("-fx-border-width: 0;");
 		profilepic.setStroke(Color.TRANSPARENT);
-		
+
 	}
 
 	public void setUserName(String username)
